2839541563d86c88ce66561330f73a6f83ed60b5
PotateSlad <haltthecavalcade@gmail.com>
2023-11-02 11:46:19 -0500

    Updated README

--- DeltaDebug.java
+++ DeltaDebug.java
@@ -1,16 +1,35 @@
+import java.util.ArrayList;
+
 // Delta Debugs a given program using the provided diff file
 class DeltaDebug {
 	public static void main(String[] args){
 		// Check to make sure all 3 args are provided
 		if (args.length != 3) {
 			System.out.println("Usage: java DeltaDebug before.java after.java diff_file");
-			exit(0);
+			System.exit(0);
 		}
 		
 		// This isn't a program writing class, so at this point we're just going to assume
 		// the correct data is supplied.
+		// TO DO: implement some sort of way to store the individual diffs for later testing
+		ArrayList<diff> diffs = new ArrayList<diff>();
+		
 		// TO DO: Open and parse the diff file. Print out change sets as in the example
 		
-		// TO DO: Binary search algo on the diff set
+		// This isn't a coverage generation project, so we'll hard code a test set of inputs here
+		String[][] tests = { {"0", "0", "summation"},
+						  {"0", "0", "multiplication"}, 
+						  {"0", "0", "minus"}, 
+						  {"0", "0", "modulous"}, 
+						  {"0", "0", "addsquare"}, 
+						  {"0", "0", "division"}, 
+						  {"0", "0", "subsquare"}, 
+						  {"0", "0", "addsubsquare"}, 
+						  {"0", "0", ""}
+		};
+		
+		// TO DO: Binary search algo as pg. 6 in the paper
+		
+		return;
 	}
 }
