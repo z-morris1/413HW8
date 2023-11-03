3386a22376136e0e779603a209c42bfa4f91cd5f
z-morris1 <149324551+z-morris1@users.noreply.github.com>
2023-11-03 10:26:46 -0500

    Add files via upload

--- DeltaDebug.java
+++ DeltaDebug.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.BufferedReader;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Scanner;
 
 // Delta Debugs a given program using the provided diff file
 class DeltaDebug {
-	public static void main(String[] args){
+	public static void main(String[] args) throws FileNotFoundException{
+
 		// Check to make sure all 3 args are provided
 		if (args.length != 3) {
 			System.out.println("Usage: java DeltaDebug before.java after.java diff_file");
@@ -19,6 +22,15 @@ class DeltaDebug {
 		ArrayList<diff> diffs = new ArrayList<diff>();
 		
 		// TO DO: Open and parse the diff file. Print out change sets as in the example
+		ArrayList<ChangeObject> ChangeObjects = new ArrayList<ChangeObject>();
+		ChangeObjects = readDiffFile();
+
+		System.out.println("Delta-debugging Project");
+		for(int i = 0; i < ChangeObjects.size(); i++){
+			System.out.println(ChangeObjects.get(i).Location);
+		}
+		System.out.println("# of Total Change sets is = " + ChangeObjects.size());
+		
 		
 		// This isn't a coverage generation project, so we'll hard code a test set of inputs here
 		String[] tests = {" 0 0 summation",
@@ -64,5 +76,78 @@ class DeltaDebug {
         }
 		pro.waitFor();
 		return;
+	}
+
+	private static ArrayList<ChangeObject> readDiffFile() throws FileNotFoundException{
+
+		ArrayList<ChangeObject> Stuff = new ArrayList<ChangeObject>();
+
+		File file = new File("diff");
+		Scanner scanner = new Scanner(file);
+
+		String Location = "";
+		String AddString = "";
+		String RemoveString = "";
+		
+		String saved = "";
+
+		saved = scanner.nextLine();
+		saved = scanner.nextLine();
+
+		while(scanner.hasNextLine()) {
+			saved = scanner.nextLine();
+			Scanner checker = new Scanner(saved);
+			String first = checker.next();
+			checker.close();
+
+			
+
+			if (first.equals("@@")) {
+				ChangeObject last = new ChangeObject(Location, AddString, RemoveString);
+				Stuff.add(last);
+
+				Location = "";
+				AddString = "";
+				RemoveString = "";
+				
+				Location = saved;
+			}
+ 
+			else if (first.equals("-")) {
+				if(RemoveString.equals("")) {
+					RemoveString = saved.substring(1, saved.length()-1);
+				}
+				else {
+					String result = "\n" + saved.substring(1, saved.length());
+					RemoveString += result;
+				}
+				
+			}
+ 
+			else if (first.equals("+")) {
+				if(AddString.equals("")) {
+					AddString = saved.substring(1, saved.length()-1);
+				}
+				else {
+					String result = "\n" + saved.substring(1, saved.length());
+					AddString += result;
+				}
+				
+			}
+
+			
+			
+
+	
+		}
+
+		ChangeObject last = new ChangeObject (Location, AddString, RemoveString);
+		Stuff.add(last);
+
+		Stuff.remove(Stuff.get(0));
+		scanner.close();
+		
+		
+		return Stuff;
 	}
 }
